16f83e1349df182b38a9b47b27063312a33ad982
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-02-29 22:53:39 -0600

    Add files via upload

---
+++ Testing.js
@@ -0,0 +1,10 @@
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+var ourStr = "This is the first sentence. ";
+ ourStr += "This is the second sentence.";
+console.log(ourStr);
+
+var lastNameLength = 0;
+var lastName = "Lovelace";
+lastNameLength = lastName.length;
+console.log(lastNameLength);
