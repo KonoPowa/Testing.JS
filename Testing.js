5bc323a77d293d45a156a6195550e8f9380a5491
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-01 22:01:02 -0600

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -1,4 +1,5 @@
 var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr);
 
 var ourStr = "This is the first sentence. ";
  ourStr += "This is the second sentence.";
@@ -8,3 +9,32 @@ var lastNameLength = 0;
 var lastName = "Lovelace";
 lastNameLength = lastName.length;
 console.log(lastNameLength);
+
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
+console.log(firstLetterOfLastName);
+
+var lastName = "Lovelace";
+var lastLetterOfLastName = lastName[lastName.length - 1];
+console.log(lastLetterOfLastName);
+
+var myArray = [18,64,99];
+myArray[0] = 45;
+console.log(myArray);
+
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+var myData = myArray[2][1];
+console.log(myData);
+
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog",3])
+console.log(myArray);
+
+var myArray = [["John", 23], ["cat", 2]];
+var removedFromMyArray = myArray.pop();
+console.log(removedFromMyArray);
+
+var myArray = [["John", 23], ["dog", 3]];
+var removedFromMyArray = myArray.shift();
+console.log(removedFromMyArray);
