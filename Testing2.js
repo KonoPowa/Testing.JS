eecf2f02174b48c4a4746bc38fcf379ff7c4165d
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-03 22:23:19 -0600

    Add files via upload

---
+++ Testing2.js
@@ -0,0 +1,16 @@
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+compareEquality(10, "10");
+console.log(compareEquality(10, "10");
+
+function testLogicalAnd(val) {
+	if (val <= 50 && val >= 25) {
+      return "Yes";
+    }
+ return "No";
+}
+console.log(testLogicalAnd(10));
