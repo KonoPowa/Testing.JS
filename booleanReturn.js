e217ec601ce28cb0344bd220e95ff37f2347c4f4
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-05 23:16:10 -0600

    Add files via upload

---
+++ booleanReturn.js
@@ -0,0 +1,93 @@
+function isLess(a, b) {
+  // Only change code below this line
+    return a < b;
+  // Only change code above this line
+}
+
+// Change these values to test
+isLess(10, 15);
+
+
+
+
+
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card){
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+     count = count + 1;
+    break;
+  case 7:
+  case 8:
+  case 9:
+     count = count + 0;
+    break;
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+     count = count - 1;
+    break;
+}
+  if (count <= 0){
+    return count + " Hold";}
+
+  else if (count > 0){
+    return count + " Bet";
+  }
+}
+  // Only change code above this line
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+
+
+
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
+
+
+
+
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+  myDog.name = "Happy Coder"
