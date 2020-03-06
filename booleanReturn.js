e8e6b03aa784a6e1b46287057cff7687c13070d9
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-06 00:10:30 -0600

    Update booleanReturn.js

--- booleanReturn.js
+++ booleanReturn.js
@@ -120,3 +120,31 @@ var myDog = {
 
 // Only change code below this line
   delete myDog.tails;
+  
+  
+  
+  
+  
+  
+  
+  
+  // Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  var lookup = {
+    "alpha":"Adams",
+    "bravo":"Boston",
+    "charlie":"Chicago",
+    "delta":"Denver",
+    "echo":"Easy",
+    "foxtrot":"Frank"
+  };
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+phoneticLookup("charlie");
