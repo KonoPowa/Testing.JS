8aa160d3d833349d7a825ebc91bf6c9681f1b533
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-05 23:44:17 -0600

    Update booleanReturn.js

--- booleanReturn.js
+++ booleanReturn.js
@@ -91,3 +91,32 @@ var myDog = {
 
 // Only change code below this line
   myDog.name = "Happy Coder"
+  
+  
+  
+  
+  
+  
+  
+  // Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+  delete myDog.tails;
