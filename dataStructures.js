2f19a1b95800a484d21f08f89df73f090166a0b0
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-06 23:00:10 -0600

    Add files via upload

---
+++ dataStructures.js
@@ -0,0 +1,23 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add a record here
+  {
+    artist: "blank",
+    title: "blank",
+    release_year: 2008,
+    formats: [
+      "youtube",
+      "CD"
+    ]
+  }
+];
