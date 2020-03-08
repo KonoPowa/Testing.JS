3f19c3a961dd0df405f92de3f854a72de31b1981
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-07 21:55:50 -0600

    Update dataStructures.js

--- dataStructures.js
+++ dataStructures.js
@@ -21,3 +21,22 @@ var myMusic = [
     ]
   }
 ];
+
+
+
+
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
