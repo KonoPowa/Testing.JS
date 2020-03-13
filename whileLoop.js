af4981c0697f9599082bd8ba1d7625d5cd0cc1bf
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-13 14:15:23 -0500

    Update whileLoop.js

--- whileLoop.js
+++ whileLoop.js
@@ -58,3 +58,22 @@ var myArray = [];
   }
 
 // Only change code below this line
+
+
+
+
+// Example
+var ourArr = [ 9, 10, 11, 12];
+var ourTotal = 0;
+
+for (var i = 0; i < ourArr.length; i++) {
+  ourTotal += ourArr[i];
+}
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+var total = 0;
+  for (i = 0; i < myArr.length; i++){
+      total += myArr[i];
+  }
+// Only change code below this line
