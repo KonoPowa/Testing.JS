de01a4222afa99b8348bf500d41fb622afd517aa
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-18 14:47:03 -0500

    Update es6Testing.js

--- es6Testing.js
+++ es6Testing.js
@@ -75,3 +75,15 @@ const magic = () => new Date();
 // Only change code below this line
 const increment = (number, value = 1) => number + value;
 // Only change code above this line
+
+
+
+
+
+
+
+const sum = (...args) => {
+
+  return args.reduce((a, b) => a + b, 0);
+}
+console.log(sum(1, 2, 3, 4)); // 6
