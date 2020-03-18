1e8b82fa18ec1eca3b599e4949375c5c9832da82
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-18 13:00:07 -0500

    Update es6Testing.js

--- es6Testing.js
+++ es6Testing.js
@@ -69,3 +69,9 @@ printManyTimes("freeCodeCamp");
 
 
 const magic = () => new Date();
+
+
+
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
