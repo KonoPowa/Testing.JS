41abd4b1a95961cbbd2a4d1a068cbcada723068d
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-29 22:22:02 -0500

    Update regularExpressions.js

--- regularExpressions.js
+++ regularExpressions.js
@@ -124,3 +124,13 @@ let result = calRegex.test(rickyAndCal);
 let caboose = "The last car on a train is the caboose";
 let lastRegex = /caboose$/; // Change this line
 let result = lastRegex.test(caboose);
+
+
+//Regular Expressions: Match All Letters and Numbers****************
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
+
+
+
+
