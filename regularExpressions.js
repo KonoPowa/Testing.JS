02303040ba18f8cf8db147f96adf00d2129e516d
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-03-31 22:42:28 -0500

    Update regularExpressions.js

--- regularExpressions.js
+++ regularExpressions.js
@@ -163,3 +163,14 @@ let result = userCheck.test(username);
 let sample = "Whitespace is important in separating words";
 let countWhiteSpace = /\s/g; // Change this line
 let result = sample.match(countWhiteSpace);
+
+
+
+//Regular Expressions: Match Non-Whitespace Characters**************************
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; // Change this line
+let result = sample.match(countNonWhiteSpace);
+
+
+
+
