cd07541fe288f4eeba972c06e444c7bc3e12d045
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-04-02 17:02:35 -0500

    Update basicDataStructures.js

--- basicDataStructures.js
+++ basicDataStructures.js
@@ -2,3 +2,16 @@
 let yourArray = ['hi',1,true,'bam','bam']; // Change this line
 
 
+
+
+//Basic Data Structures: Access an Array's Contents Using Bracket Notation***************
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "skadoosh"
+// Only change code above this line
+console.log(myArray);
+
+
+
+
+
