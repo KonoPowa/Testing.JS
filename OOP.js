df724e85dcbcafb259ee300db4ce6ead0d67e2ae
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-04-10 17:53:57 -0500

    Update OOP.js

--- OOP.js
+++ OOP.js
@@ -103,3 +103,22 @@ for (let property in canary){
 
   }
 }
+
+
+
+
+
+
+
+
+
+
+//Object Oriented Programming: Use Prototype Properties to Reduce Duplicate Code*************
+function Dog(name) {
+  this.name = name;
+}
+Dog.prototype.numLegs = 4;
+
+
+// Only change code above this line
+let beagle = new Dog("Snoopy");
