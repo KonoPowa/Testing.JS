bafb64d9396985704017b75160fbe10b83650f4b
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-04-10 18:17:30 -0500

    Update OOP.js

--- OOP.js
+++ OOP.js
@@ -175,3 +175,27 @@ function joinDogFraternity(candidate) {
     return false
   }
 }
+
+
+
+
+
+
+
+
+
+//Object Oriented Programming: Change the Prototype to a New Object**************************
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+numLegs: 4,
+ eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
