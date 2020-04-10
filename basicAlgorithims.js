39d3e9c15f063f0b1cb54f9406b7cc8f6d1eb136
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-04-09 20:51:12 -0500

    Update basicAlgorithims.js

--- basicAlgorithims.js
+++ basicAlgorithims.js
@@ -44,3 +44,11 @@ function Dog(){
 
 
 
+//Object Oriented Programming: Use a Constructor to Create Objects****************************
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Only change code below this line
+let hound = new Dog();
