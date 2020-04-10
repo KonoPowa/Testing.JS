bd6c5c1df1eb09de372037f7f43325a74282dbc0
KonoPowa <50770569+KonoPowa@users.noreply.github.com>
2020-04-10 14:22:47 -0500

    Add files via upload

---
+++ OOP.js
@@ -0,0 +1,105 @@
+//Object Oriented Programming: Create a Method on an Object*********
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {return "This dog has 4 legs."}
+};
+
+dog.sayLegs();
+
+
+
+
+
+
+
+//Object Oriented Programming: Make Code More Reusable with the this Keyword*************************
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+};
+
+dog.sayLegs();
+
+
+
+
+
+
+
+
+
+
+//Object Oriented Programming: Define a Constructor Function*********************************
+function Dog(){
+  this.name = "fido";
+  this.color = "red";
+  this.numLegs = 4;
+}
+
+
+
+
+
+
+
+//Object Oriented Programming: Use a Constructor to Create Objects****************************
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Only change code below this line
+let hound = new Dog();
+
+
+
+
+
+
+//Object Oriented Programming: Extend Constructors to Receive Arguments***********************
+function Dog(name,color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 4;
+}
+let terrier = new Dog("buddy", "red")
+
+
+
+
+
+
+
+//Object Oriented Programming: Verify an Object's Constructor with instanceof***************************
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+// Only change code below this line
+let myHouse = new House(5);
+myHouse instanceof House;
+
+
+
+
+
+
+
+
+//Object Oriented Programming: Understand Own Properties*************************
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Only change code below this line
+for (let property in canary){
+  if(canary.hasOwnProperty(property)){
+    ownProps.push(property);
+
+  }
+}
