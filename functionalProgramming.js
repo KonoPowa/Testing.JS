4f35bd5f6e243ebc1c6cf742ab43728dd9a3d54b
Yacob Abusadah <50770569+KonoPowa@users.noreply.github.com>
2020-04-16 12:44:46 -0500

    Update functionalProgramming.js

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -529,3 +529,23 @@ function nonMutatingConcat(original, attach) {
 var first = [1, 2, 3];
 var second = [4, 5];
 nonMutatingConcat(first, second);
+
+
+
+
+
+
+
+
+
+
+//Functional Programming: Add Elements to the End of an Array Using concat Instead of push*****************
+function nonMutatingPush(original, newItem) {
+  // Only change code below this line
+  return original.concat(newItem);
+
+  // Only change code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingPush(first, second);
