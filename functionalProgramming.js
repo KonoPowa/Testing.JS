cee631d244384cd1555e1815ed2d1736569c2b5a
Yacob Abusadah <50770569+KonoPowa@users.noreply.github.com>
2020-04-17 12:26:37 -0500

    Update functionalProgramming.js

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -879,3 +879,34 @@ function checkPositive(arr) {
   // Only change code above this line
 }
 checkPositive([1, 2, 3, -4, 5]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Functional Programming: Introduction to Currying and Partial Application****************************
+function add(x) {
+  // Only change code below this line
+  return function(y){
+    return function(z){
+      return x + y + z
+    }
+  }
+
+  // Only change code above this line
+}
+add(10)(20)(30);
