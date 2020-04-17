73e2904f83f9b838d7b12b4248af249951083748
Yacob Abusadah <50770569+KonoPowa@users.noreply.github.com>
2020-04-17 12:20:18 -0500

    Update functionalProgramming.js

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -854,3 +854,28 @@ function checkPositive(arr) {
 }
 checkPositive([1, 2, 3, -4, 5]);
 console.log(checkPositive([1, 2, 3, -4, 5]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Functional Programming: Use the some Method to Check that Any Elements in an Array Meet a Criteria************************
+function checkPositive(arr) {
+  // Only change code below this line
+  return arr.some(function(currentValue){
+    return currentValue > 0
+  })
+
+  // Only change code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
